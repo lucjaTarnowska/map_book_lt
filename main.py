7e80aa6bbf56a67c361f0d4f4afcb85e8b0b42e2
lucjaTarnowska <163434806+lucjaTarnowska@users.noreply.github.com>
2024-04-16 18:14:21 +0200

    wip: add new menu function, add user append

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models.data_source import users
-from utils.crud import read_friends
+from utils.crud import read_friends, add_user
 
 
 
@@ -9,8 +9,11 @@ if __name__ == '__main__':
         print("Welcome to the menu choose on option")
         print("1. Read a list of friends")
         print("0. Exit ")
+        print("2. Add new user")
         menu_option = input("Choose an option")
         if menu_option == "0":
             break
         if menu_option == "1":
             read_friends(users)
+        if menu_option == "2":
+            add_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -2,3 +2,10 @@ def read_friends(users: list) -> None:
     print("informacje o twoich znajomych: ")
     for user in users:
         print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+
+def add_user(lista: list) -> None:
+    imie = input("podaj imie: ")
+    nazwisko = input("podaj nazwisko: ")
+    liczba_postow = int(input("podaj liczbe postów użytkownika: "))
+    new_user = {"name": imie, "surname": nazwisko, "posts": liczba_postow, }
+    lista.append(new_user)
