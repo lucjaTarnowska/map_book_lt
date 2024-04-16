0773ff3f697df12fd7f457d53a4789783aee4532
lucjaTarnowska <163434806+lucjaTarnowska@users.noreply.github.com>
2024-04-16 17:24:18 +0200

    feat: read code refactor

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ users: list = [
     {"name": "Julia", "surname": "Gotowiec", "posts": 1500, },
     {"name": "Hubert", "surname": "Sybilski", "posts": 123456, },
     {"name": "Adrian", "surname": "Dobrzański", "posts": 3, }
+
 ]
 
-print("informacje o twoich znajomych: ")
-for user in users:
-    print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+def read_friends(users: list)->None:
+    print("informacje o twoich znajomych: ")
+    for user in users:
+        print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+
+
+read_friends(users)
