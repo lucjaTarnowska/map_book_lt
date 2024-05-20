c171840f6de8c6a279e756f961012acd6b755212
lucjaTarnowska <163434806+lucjaTarnowska@users.noreply.github.com>
2024-05-20 16:18:12 +0200

    feat: add crud for map

--- notatnik.py
+++ notatnik.py
@@ -1,29 +1,25 @@
-import requests
-from bs4 import BeautifulSoup
+import psycopg2 as ps
 
 
-class User:
-    def __init__(self, name, surname, posts, location):
-        self.name = name
-        self.surname = surname
-        self.posts = posts
-        self.location = location
-        self.wspolrzedne = User.wspolrzedne(self)
+db_params = ps.connect(
+    database="map_book",
+    user="postgres",
+    password="geoinformatyka",
+    host="localhost",
+    port="5432"
+)
+
+cursor = db_params.cursor()
+sql_show_users = "SELECT * FROM public.users"
+cursor.execute(sql_show_users)
+query_result = cursor.fetchall()
+for row in query_result:
+	print(row[0])
+	print(row[1])
+	print(row[2])
+	print(row[3])
 
 
-    def wspolrzedne(self)-> list:
-        url: str = f'https://pl.wikipedia.org/wiki/{self.location}'
-        response = requests.get(url)
-        response_html = BeautifulSoup(response.text, 'html.parser')
-        latitude = response_html.select('.latitude')[1].text.replace(",", ".")
-        longitude = response_html.select('.longitude')[1].text.replace(",", ".")
-        return [latitude, longitude]
-
-
-
-#https: // pl.wikipedia.org / wiki / Zambrów
-
-lucja=User("Lucja", "Tarnowska", posts="10", location="Zambrów")
-print(type(lucja.wspolrzedne[0]))
-
-
+# INSERT INTO public.users(
+#   name, surname, posts, location, wspolrzedne)
+#   VALUES ( 'Karol', 'Makowski', '10', 'Warszawa', 'SRID=4326;POINT(52.21 21.00)');
